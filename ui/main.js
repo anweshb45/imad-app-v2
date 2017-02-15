106fb95d4c68ee7329eb119414f4b287da5b75ac
anweshb45 <anweshb45@gmail.com>
2017-02-15 22:57:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick=function(){
     counter = counter + 1;
-    var span = document.getElementById('count');
+    var span = documentgetElementById('count');
     span.innerHTML = counter.toString();
 };
