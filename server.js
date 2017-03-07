de0ff880f326c39813ea1ab995344ff65a4ab603
anweshb45 <anweshb45@gmail.com>
2017-03-07 21:29:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','main.js'));
+    res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 app.get('/article-one',function(req,res){
