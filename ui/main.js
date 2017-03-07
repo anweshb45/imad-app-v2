8153c29d73f42e81105a9c934d7e2564f87c6a12
anweshb45 <anweshb45@gmail.com>
2017-03-07 22:22:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick=function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
@@ -8,7 +8,7 @@ button.onclick=function(){
             if(request.status === 200){
                     var counter = request.responseText;
                     var span = document.getElementById('count');
-                    span.innerHTML = counter;
+                    span.innerHTML = counter.toString();
             }
         }
         
