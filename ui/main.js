5cd45067506dfbf82a80c93a1b2bd943173bccfb
anweshb45 <anweshb45@gmail.com>
2017-03-08 21:00:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,5 +15,16 @@ button.onclick=function(){
     };
     request.open('GET','http://anweshb45.imad.hasura-app.io/counter',true);
     request.send(null);
-
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function(){
+        var names = [name1,name2,name3,name4];
+        var list = '';
+        for( int i=0; i<names.length;i++){
+            list += <li> + names[i] + </li>;
+        }
+        var ul = getElementById('namelist');
+        ul.innnerHTML = list;
+    }
 };
