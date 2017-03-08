717e02180da42c43ba0e84674451811ba944d5de
anweshb45 <anweshb45@gmail.com>
2017-03-08 22:14:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,22 +9,24 @@ button.onclick=function(){
                     var counter = request.responseText;
                     var span = document.getElementById('count');
                     span.innerHTML = counter.toString();
+                    var nameInput = document.getElementById('name');
+                    var name = nameInput.value;
+                    var submit = document.getElementById('submit_btn');
+                    submit.onclick = function(){
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    for( var i=0; i<names.length;i++){
+                        list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                };
             }
         }
         
     };
     request.open('GET','http://anweshb45.imad.hasura-app.io/counter',true);
+    request.open('GET','http://anweshb45.imad.hasura-app.io/submit-name' + name,true);
     request.send(null);
-};
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for( var i=0; i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 };
