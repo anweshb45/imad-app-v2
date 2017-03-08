6df41a6addf0088e1362d4f1104227adf3d69d91
anweshb45 <anweshb45@gmail.com>
2017-03-08 22:38:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,6 @@ submit.onclick = function(){
         }
         
     };
-    request.open('GET','http://anweshb45.imad.hasura-app.io/submit-name' + name ,true);
+    request.open('GET','http://anweshb45.imad.hasura-app.io/submit-name?name=' + name ,true);
     request.send(null);
 };
