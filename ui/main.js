335ec46999c36a5812a37cda78d688b6a82050dc
anweshb45 <anweshb45@gmail.com>
2017-03-08 21:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,8 @@ button.onclick=function(){
     submit.onclick = function(){
         var names = [name1,name2,name3,name4];
         var list = '';
-        for( int i=0; i<names.length;i++){
-            list += <li> + names[i] + </li>;
+        for( var i=0; i<names.length;i++){
+            list += '<li>' + names[i] + '</li>';
         }
         var ul = getElementById('namelist');
         ul.innnerHTML = list;
