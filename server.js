7c9fd8243e176bea3bde9b0ad69e884a67a53fb1
anweshb45 <anweshb45@gmail.com>
2017-03-15 22:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test.db',function(req,res){
-    pool.query('SELECT * FROM TEST',function(err,result){
+    pool.query('SELECT * FROM test',function(err,result){
         if(err){
             res.status(500).send(toSTring(err));
         } else{
